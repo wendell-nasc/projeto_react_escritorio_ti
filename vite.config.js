c79b6406203f74238f6e4bcc599f0170bd2676c1
wendell silva <wendell@wendells-MacBook-Pro.local>
2025-09-05 13:45:30 -0300

    ¨onze¨

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // ✅ Garante que os assets sejam carregados a partir da raiz do servidor
+  build: {
+    outDir: "dist", // Diretório de saída do build
+    assetsDir: "assets", // Pasta para os arquivos estáticos
+  },
+});

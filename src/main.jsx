7ee14fc9e74d174bbf7a4765ae77d1f89c118516
wendell silva <wendell@wendells-MacBook-Pro.local>
2025-09-08 12:24:35 -0300

    cinquenta e um

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-import "./widgets/meu-widget.js"; // Importa o Web Component
+// import "./widgets/meu-widget.js"; // Importa o Web Component
 
 
 // Correto: JSX com comentário de bloco

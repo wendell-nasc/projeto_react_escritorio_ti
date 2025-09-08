9dcd0c3814279d88f8f340e84d00fd6317794801
wendell silva <wendell@wendells-MacBook-Pro.local>
2025-09-08 12:15:02 -0300

    cinquenta

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-// import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client';
-import { App } from './App.jsx'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
+import "./widgets/meu-widget.js"; // Importa o Web Component
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+// Correto: JSX com comentário de bloco
+createRoot(document.getElementById('root')).render(
+  <>
+    {/* <StrictMode> */}
+    <App />    
+    {/* </StrictMode> */}
+  </>
 )
-
